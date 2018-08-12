c28d89911cfc3e8e1f24aae15e1e9d9e9e46ba57
Lokhas <m.vandewaetere@hotmail.fr>
2018-08-13 00:32:08 +0200

    improved accessibility

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ let urlsToCache = [
   "/js/dbhelper.js",
   "/js/main.js",
   "/js/restaurant_info.js",
-  "/sw_registration.js",
+  "/js/sw_registration.js",
   "/data/restaurants.json",
   "/img/1.jpg",
   "/img/2.jpg",
